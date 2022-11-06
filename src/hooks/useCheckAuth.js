d7fa2169d1d54a89dd704a199e3df536cdf2b508
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2022-11-06 14:29:23 -0300

    Se agrega spinner cuando esté checkeando login

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
+
 import { FirebaseAuth } from "../firebase/config";
 import { login, logout } from "../store/auth";
 
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const authSlice = createSlice({
    name: 'auth',
    initialState: { 
-        status: 'non-authenticated' // 'checking', 'non-authenticated', 'authenticated' 
+        status: 'checking' // 'checking', 'non-authenticated', 'authenticated' 
     },
    reducers: {
         login: (state, { payload }) => {
